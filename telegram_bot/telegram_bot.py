dd0bb8beee11765bd1fc709dae9d2c06b5f2178d
ViciaCare <pvk.sakha@yandex.ru>
2021-06-18 17:26:33 +0900

    edded functions name, surname, age

--- telegram_bot/telegram_bot.py
+++ telegram_bot/telegram_bot.py
@@ -1,6 +1,10 @@
 import telebot
 import config
 
+name = ''
+surname = ''
+age = 0
+
 bot = telebot.TeleBot(config.TOKEN)
 
 @bot.message_handler(commands=['start', 'help'])
@@ -9,6 +13,38 @@ def send_welcome(message):
 
 @bot.message_handler(func=lambda m: True)
 def echo_all(message):
-    bot.reply_to(message, message.text)
+    if message.text == "Привет":
+        bot.reply_to(message, "Привет, человек!")
+    elif message.text == "Hi":
+        bot.reply_to(message, "Hi! My creator")
+    elif message.text == '/reg':
+        bot.send_message(message.from_user.id, "Привет! Давай познакомимся! Как тебя зовут?")
+        bot.register_next_step_handler(message, reg_name)
+
+    #bot.reply_to(message, message.text)
+
+def reg_name(message):
+    global name
+    name = message.text
+    bot.send_message(message.from_user.id, "Какая у тебя фамилия?")
+    bot.register_next_step_handler(message, reg_surname)
+
+
+def reg_surname(message):
+    global surname
+    surname = message.text
+    bot.send_message(message.from_user.id, "Сколько тебе лет?")
+    bot.register_next_step_handler(message, reg_age)
+
+
+def reg_age(message):
+    global age
+    #age = message.text
+    while age == 0:
+        try:
+            age = int(message.text)
+        except Exceptiona:
+            bot.send_message(message.from_user.id, "Вводите пожалуйста цифрами!")
+
 
 bot.polling()
