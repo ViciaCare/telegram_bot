cfeced19c5c8a26ef749cd6f2b0b302c695805cc
ViciaCare <pvk.sakha@yandex.ru>
2021-06-18 18:10:57 +0900

    edded keyboard and function callback

--- telegram_bot/telegram_bot.py
+++ telegram_bot/telegram_bot.py
@@ -1,4 +1,5 @@
 import telebot
+from telebot import types
 import config
 
 name = ''
@@ -7,9 +8,11 @@ age = 0
 
 bot = telebot.TeleBot(config.TOKEN)
 
+
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
     bot.reply_to(message, "Howdy, how are you doing?")
+
 
 @bot.message_handler(func=lambda m: True)
 def echo_all(message):
@@ -20,8 +23,8 @@ def echo_all(message):
     elif message.text == '/reg':
         bot.send_message(message.from_user.id, "Привет! Давай познакомимся! Как тебя зовут?")
         bot.register_next_step_handler(message, reg_name)
+    # bot.reply_to(message, message.text)
 
-    #bot.reply_to(message, message.text)
 
 def reg_name(message):
     global name
@@ -39,12 +42,30 @@ def reg_surname(message):
 
 def reg_age(message):
     global age
-    #age = message.text
+    # age = message.text
     while age == 0:
         try:
             age = int(message.text)
-        except Exceptiona:
+        except Exception:
             bot.send_message(message.from_user.id, "Вводите пожалуйста цифрами!")
+
+    keyboard = types.InlineKeyboardMarkup()
+    key_yes = types.InlineKeyboardButton(text='Да', callback_data='yes')
+    keyboard.add(key_yes)
+    key_no = types.InlineKeyboardButton(text='Нет', callback_data='no')
+    keyboard.add(key_no)
+    question = "Тебе " + str(age) + " лет? И тебя зовут: " + name + " " + surname + "?"
+    bot.send_message(message.from_user.id, text=question, reply_markup=keyboard)
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_worker(call):
+    if call.data == "yes":
+        bot.send_message(call.message.chat.id, "Приятно познакомиться! Запомню!")
+    elif call.data == "no":
+        bot.send_message(call.message.chat.id, "Попробуем еще раз!")
+        bot.send_message(call.from_user.id, "Привет! Давай познакомимся! Как тебя зовут?")
+        bot.register_next_step_handler(call.message, reg_name)
 
 
 bot.polling()
