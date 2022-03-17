0f419b3a7087f4006d3960d6456a145a0e80e2d4
ViciaCare <pvk.sakha@yandex.ru>
2022-03-17 10:13:25 +0900

    changed massage

--- telegram_bot/telegram_bot.py
+++ telegram_bot/telegram_bot.py
@@ -1,18 +1,16 @@
 import telebot
 from telebot import types
-import config
+import settings
 
 name = ''
 surname = ''
 age = 0
 
-bot = telebot.TeleBot(config.TOKEN)
-
+bot = telebot.TeleBot(settings.TOKEN)
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    bot.reply_to(message, "Howdy, how are you doing?")
-
+    bot.reply_to(message, "How are you doing?")
 
 @bot.message_handler(func=lambda m: True)
 def echo_all(message):
@@ -25,20 +23,17 @@ def echo_all(message):
         bot.register_next_step_handler(message, reg_name)
     # bot.reply_to(message, message.text)
 
-
 def reg_name(message):
     global name
     name = message.text
     bot.send_message(message.from_user.id, "Какая у тебя фамилия?")
     bot.register_next_step_handler(message, reg_surname)
 
-
 def reg_surname(message):
     global surname
     surname = message.text
     bot.send_message(message.from_user.id, "Сколько тебе лет?")
     bot.register_next_step_handler(message, reg_age)
-
 
 def reg_age(message):
     global age
@@ -57,7 +52,6 @@ def reg_age(message):
     question = "Тебе " + str(age) + " лет? И тебя зовут: " + name + " " + surname + "?"
     bot.send_message(message.from_user.id, text=question, reply_markup=keyboard)
 
-
 @bot.callback_query_handler(func=lambda call: True)
 def callback_worker(call):
     if call.data == "yes":
@@ -66,6 +60,5 @@ def callback_worker(call):
         bot.send_message(call.message.chat.id, "Попробуем еще раз!")
         bot.send_message(call.from_user.id, "Привет! Давай познакомимся! Как тебя зовут?")
         bot.register_next_step_handler(call.message, reg_name)
-
 
 bot.polling()
